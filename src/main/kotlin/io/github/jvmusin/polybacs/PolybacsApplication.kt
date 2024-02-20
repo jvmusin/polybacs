089fe9c92abfe7b9832bd5d756a9b07997225098
Rustam Musin <jvmusin@gmail.com>
2024-02-20 21:55:46 +0000

    Add /ws handler

--- src/main/kotlin/io/github/jvmusin/polybacs/PolybacsApplication.kt
+++ src/main/kotlin/io/github/jvmusin/polybacs/PolybacsApplication.kt
@@ -1,13 +1,22 @@
 package io.github.jvmusin.polybacs
 
-import jakarta.servlet.http.Cookie
-import jakarta.servlet.http.HttpServletRequest
-import jakarta.servlet.http.HttpServletResponse
+import org.apache.catalina.Context
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDateTime
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
+import org.springframework.web.socket.CloseStatus
+import org.springframework.web.socket.PongMessage
+import org.springframework.web.socket.TextMessage
+import org.springframework.web.socket.WebSocketSession
+import org.springframework.web.socket.config.annotation.EnableWebSocket
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+import org.springframework.web.socket.handler.TextWebSocketHandler
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor
 
 @SpringBootApplication
 class PolybacsApplication
@@ -16,28 +25,58 @@ fun main(args: Array<String>) {
     runApplication<PolybacsApplication>(*args)
 }
 
-@RestController
-class HelloController {
-    @GetMapping("/")
-    fun hello(request: HttpServletRequest, response: HttpServletResponse) {
-        // get cookie
-        val cookies = request.cookies
-        if (cookies == null || cookies.isEmpty()) println("NO COOKIES")
-        else cookies.forEach { println("COOKIES: " + it.name + "=" + it.value) }
+@Component
+class WebSocketConnectionKeeper : TextWebSocketHandler() {
+    private val handlers = hashMapOf<String, MutableList<WebSocketSession>>()
 
-        // set cors to allow everything
-        response.addHeader("Access-Control-Allow-Origin", "*")
-        response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-        response.addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
-        response.addHeader("Access-Control-Max-Age", "3600")
+    override fun afterConnectionEstablished(session: WebSocketSession) {
+        handlers.getOrPut(session.sessionId) { mutableListOf() }.add(session)
+        println("Connection established with session: ${session.sessionId} with id: ${session.id}, active sessions: ${handlers.values.sumOf { it.size }}")
+    }
 
-        // Set cookie "poly" to current time, and make it SameSite=None; Secure
-        response.addCookie(Cookie("poly", LocalDateTime.now().toString()).apply {
-            isHttpOnly = true
-            maxAge = 3600
-            secure = true
-            this.setAttribute("SameSite", "None")
-            this.setAttribute("Partitioned", "true")
-        })
+    override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
+        handlers[session.sessionId]?.remove(session)
+        println("Connection closed with session: ${session.sessionId} with id: ${session.id}, active sessions: ${handlers.values.sumOf { it.size }}, reason: $status")
+    }
+
+    private val WebSocketSession.sessionId: String
+        get() = this.attributes[HttpSessionHandshakeInterceptor.HTTP_SESSION_ID_ATTR_NAME] as? String
+            ?: throw IllegalStateException("No session id found in WebSocketSession attributes")
+
+    public override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
+        println("Received message: " + message.payload + " from session: ${session.sessionId} with id: ${session.id}")
+    }
+
+    override fun handlePongMessage(session: WebSocketSession, message: PongMessage) {
+        println("Received pong message from session: ${session.sessionId} with id: ${session.id}")
+        super.handlePongMessage(session, message)
+    }
+}
+
+@Configuration
+@EnableWebSocket
+class WebSocketConfig(
+    private val connectionKeeper: WebSocketConnectionKeeper,
+) : WebSocketConfigurer {
+    override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
+        registry
+            .addHandler(connectionKeeper, "/ws")
+            .setAllowedOrigins("*")
+            .addInterceptors(HttpSessionHandshakeInterceptor().apply {
+                isCreateSession = true
+            })
+    }
+
+    @Bean
+    fun tomcatContextCustomizer(): TomcatContextCustomizer {
+        val cookieProcessor = Rfc6265CookieProcessor()
+        cookieProcessor.setSameSiteCookies("None")
+        cookieProcessor.partitioned = true
+        // Would be great if we could set "Secure" here and not in application.yaml
+        // For the Partitioned support see https://github.com/spring-projects/spring-framework/issues/31454
+        return TomcatContextCustomizer { context: Context ->
+            context.usePartitioned = true
+            context.cookieProcessor = cookieProcessor
+        }
     }
 }
