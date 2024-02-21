a0b94945aebab85e7d5533f1c5b97f2e173184c8
Rustam Musin <jvmusin@gmail.com>
2024-02-21 13:10:53 +0000

    Add /test endpoint for testing websocket connection

---
+++ src/main/kotlin/io/github/jvmusin/polybacs/server/WebSocketTestController.kt
@@ -0,0 +1,21 @@
+package io.github.jvmusin.polybacs.server
+
+import io.github.jvmusin.polybacs.WebSocketConnectionKeeper
+import io.github.jvmusin.polybacs.fixCors
+import jakarta.servlet.http.HttpServletResponse
+import jakarta.servlet.http.HttpSession
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping
+class WebSocketTestController(
+    private val webSocketConnectionKeeper: WebSocketConnectionKeeper,
+) {
+    @GetMapping("/test")
+    fun test(session: HttpSession, response: HttpServletResponse) {
+        webSocketConnectionKeeper.createSender(session.id).send("Hey you! This is a pong!")
+        response.fixCors()
+    }
+}
