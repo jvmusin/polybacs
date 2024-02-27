2a57accef2895bddb808151aab6d84eb1d755d86
Rustam Musin <jvmusin@gmail.com>
2024-02-27 21:36:15 +0000

    Enable slow 3+ minutes integration test

--- src/test/kotlin/io/github/jvmusin/polybacs/sybon/SybonSpecialCollectionTests.kt
+++ src/test/kotlin/io/github/jvmusin/polybacs/sybon/SybonSpecialCollectionTests.kt
@@ -5,8 +5,10 @@ import io.github.jvmusin.polybacs.bacs.BacsArchiveService
 import io.github.jvmusin.polybacs.polygon.PolygonService
 import io.github.jvmusin.polybacs.sybon.api.SybonArchiveApi
 import io.kotest.core.spec.style.StringSpec
+import kotlinx.coroutines.delay
 import org.springframework.boot.test.context.SpringBootTest
 import java.time.LocalDateTime
+import kotlin.time.Duration.Companion.minutes
 
 @SpringBootTest
 class SybonSpecialCollectionTests(
@@ -16,11 +18,12 @@ class SybonSpecialCollectionTests(
 ) : StringSpec({
     val specialCollectionId = 10023
     val polygonProblemId = 147360
-    val properties = AdditionalProblemProperties(suffix = LocalDateTime.now().run { "-$hour-$minute" })
+    val properties = AdditionalProblemProperties(suffix = LocalDateTime.now().run { "-$hour-$minute-test-rustam" })
 
-    "!Full cycle" {
-        val irProblem = polygonService.downloadProblem(polygonProblemId)
+    "Full cycle" {
+        val irProblem = polygonService.downloadProblem(polygonProblemId, includeTests = true)
         val fullName = bacsArchiveService.uploadProblem(irProblem, properties)
+        delay(3.minutes) // wait until the problem is indexed by BACS
         val sybonProblemId = sybonArchiveApi.importProblem(specialCollectionId, fullName)
         println(fullName)
         println(sybonProblemId)
