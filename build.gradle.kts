ad15844fe923ece5cc4e382d1c6ca1a64be3addd
Rustam Musin <jvmusin@gmail.com>
2024-03-01 16:21:42 +0000

    Add Dockerfile.native

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.2.3"
 	id("io.spring.dependency-management") version "1.1.4"
-//	id("org.graalvm.buildtools.native") version "0.10.1" // Enable when bellsoft/liberica-native-image-kit-container for JDK 21 is available
-	// https://hub.docker.com/r/bellsoft/liberica-native-image-kit-container/tags?page=1&name=jdk-21
+	id("org.graalvm.buildtools.native") version "0.10.1"
 	kotlin("jvm") version "1.9.22"
 	kotlin("plugin.spring") version "1.9.22"
 }
