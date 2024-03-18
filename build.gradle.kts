8386dc81f2f580bfbe9b4e5d82267bd759361177
Rustam <jvmusin@gmail.com>
2024-03-18 12:28:31 +0100

    Reformat build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,22 +2,22 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.3"
-	id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
 //	id("org.graalvm.buildtools.native") version "0.10.1"
-	kotlin("jvm") version "1.9.23"
-	kotlin("plugin.spring") version "1.9.23"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
 }
 
 group = "io.github.jvmusin"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 val jsoupVersion = "1.17.2"
@@ -25,55 +25,55 @@ val kotestVersion = "5.8.1"
 val kotestExtensionsSpringVersion = "1.1.3"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-websocket")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	testImplementation("io.projectreactor:reactor-test")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    testImplementation("io.projectreactor:reactor-test")
 
-	implementation("org.jsoup:jsoup:$jsoupVersion")
+    implementation("org.jsoup:jsoup:$jsoupVersion")
 
-	testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
-	testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
-	testImplementation("io.kotest.extensions:kotest-extensions-spring:$kotestExtensionsSpringVersion")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:$kotestExtensionsSpringVersion")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "21"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "21"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-	testLogging {
-		exceptionFormat = TestExceptionFormat.FULL
-	}
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+    }
 }
 
 if (project.hasProperty("buildFrontend")) {
-	val buildFrontend = tasks.register<Exec>("buildFrontend") {
-		group = "build"
-		description = "Builds the frontend."
-		commandLine = listOf("sh", "-c", "cd frontend && npm ci && npm run build")
-		dependsOn("compileKotlin")
-	}
-	val copyFrontend = tasks.register<Copy>("copyFrontend") {
-		group = "build"
-		description = "Copies the frontend build to the resources."
-		from("frontend/dist")
-		into("build/resources/main/static")
-		dependsOn(buildFrontend)
-	}
-	tasks.named<ProcessResources>("processResources") {
-		dependsOn(copyFrontend)
-		exclude("static/index.html")
-	}
+    val buildFrontend = tasks.register<Exec>("buildFrontend") {
+        group = "build"
+        description = "Builds the frontend."
+        commandLine = listOf("sh", "-c", "cd frontend && npm ci && npm run build")
+        dependsOn("compileKotlin")
+    }
+    val copyFrontend = tasks.register<Copy>("copyFrontend") {
+        group = "build"
+        description = "Copies the frontend build to the resources."
+        from("frontend/dist")
+        into("build/resources/main/static")
+        dependsOn(buildFrontend)
+    }
+    tasks.named<ProcessResources>("processResources") {
+        dependsOn(copyFrontend)
+        exclude("static/index.html")
+    }
 }
