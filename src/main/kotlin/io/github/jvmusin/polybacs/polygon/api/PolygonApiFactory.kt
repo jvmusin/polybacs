3fd7c10a81926addc60bbe00b1d6dc067ddae258
Rustam <jvmusin@gmail.com>
2024-05-10 21:34:29 +0200

    Add logRequestsFilter

--- src/main/kotlin/io/github/jvmusin/polybacs/polygon/api/PolygonApiFactory.kt
+++ src/main/kotlin/io/github/jvmusin/polybacs/polygon/api/PolygonApiFactory.kt
@@ -2,18 +2,19 @@ package io.github.jvmusin.polybacs.polygon.api
 
 import io.github.jvmusin.polybacs.polygon.PolygonConfig
 import io.github.jvmusin.polybacs.util.sha512
+import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.codec.ClientCodecConfigurer
-import org.springframework.web.reactive.function.client.ClientRequest
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction
-import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.BodyExtractors
+import org.springframework.web.reactive.function.client.*
 import org.springframework.web.reactive.function.client.support.WebClientAdapter
 import org.springframework.web.service.invoker.HttpServiceProxyFactory
 import org.springframework.web.service.invoker.createClient
 import org.springframework.web.util.UriComponentsBuilder
+import reactor.core.publisher.Flux
 import java.net.URLDecoder
 
 @Configuration
@@ -45,8 +46,32 @@ class PolygonApiFactory(
      * Polygon API returns `text/html` content type, but it's actually `application/json`.
      */
     private val fixResponseContentTypeFilter = ExchangeFilterFunction { request, next ->
+        if (request.url().toString().contains("problem.testAnswer")) {
+            return@ExchangeFilterFunction next.exchange(request)
+        }
         next.exchange(request).map {
             it.mutate().headers { headers -> headers.contentType = MediaType.APPLICATION_JSON }.build()
+        }
+    }
+
+    val logRequestFilter = object : CoExchangeFilterFunction() {
+        override suspend fun filter(request: ClientRequest, next: CoExchangeFunction): ClientResponse {
+            if (false) {
+                val url = request.url()
+                val exchange = next.exchange(request)
+                try {
+                    val body1 = exchange.body(BodyExtractors.toDataBuffers())
+                    val b = body1.collectList().awaitSingle()
+                    println(b)
+                    println(url)
+                    return exchange.mutate().body(Flux.empty()).build()
+                } catch (e: Exception) {
+                    println(e.message)
+                    e.printStackTrace()
+                    throw e
+                }
+            }
+            return next.exchange(request)
         }
     }
 
@@ -83,6 +108,7 @@ class PolygonApiFactory(
             .filter(fixResponseContentTypeFilter)
             .filter(responseCode400to200Filter)
             .filter(insertApiSigFilter)
+            .filter(logRequestFilter)
         // TODO: Add 500 and 429 retry filters
         //  429 is actually 400 with text in the body
         //  TOO_MANY_REQUESTS_MESSAGE = "Too many requests. Please, wait few seconds and try again"
