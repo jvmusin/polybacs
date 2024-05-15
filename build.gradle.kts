1fc874769bb7763b2342957d0f348ede25919917
Rustam <jvmusin@gmail.com>
2024-05-15 22:12:54 +0200

    Upgrade to Kotlin 2.0.0-RC3

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.5"
 	id("org.graalvm.buildtools.native") version "0.10.1"
-    kotlin("jvm") version "1.9.24"
-    kotlin("plugin.spring") version "1.9.24"
+    kotlin("jvm") version "2.0.0-RC3"
+    kotlin("plugin.spring") version "2.0.0-RC3"
 }
 
 group = "io.github.jvmusin"
