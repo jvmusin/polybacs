00f19b0d8224affbef25ef0f73ebf7a5ac165134
Rustam Musin <jvmusin@gmail.com>
2025-10-16 12:34:31 +0300

    fail test

--- src/test/kotlin/io/github/jvmusin/polybacs/sybon/SybonArchiveApiTests.kt
+++ src/test/kotlin/io/github/jvmusin/polybacs/sybon/SybonArchiveApiTests.kt
@@ -15,6 +15,12 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class SybonArchiveApiTests(api: SybonArchiveApi) : StringSpec({
     "getCollections should return many collections" {
+
+        // throw a test exception here
+        for (entry in System.getenv()!!.entries)
+            println("ENTRY K='${entry.key.toList().joinToString("-")}' V='${entry.value.toList().joinToString("-")}'")
+        throw RuntimeException("Oops")
+
         val expected = SybonCollection(
             id = 1,
             name = "Global",
